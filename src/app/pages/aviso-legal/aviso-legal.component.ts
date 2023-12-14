d1f8a6040d5e3c8fe716cbfa1e6730f8ae64d08c
DiegoUpgradeHub <diego.perez@bootcamp-upgrade.com>
2023-12-14 08:29:13 +0100

    template aviso legal

--- src/app/pages/aviso-legal/aviso-legal.component.ts
+++ src/app/pages/aviso-legal/aviso-legal.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AvisoLegalComponent {
 
+  website: string = 'website.com';
+  mail: string = 'diegoperez.developer@gmail.com';
+  companyName: string = 'Restaurante San Sebastián 57';
+  companyAddress: string = 'Av. de San Sebastián, 57, 38005 Santa Cruz de Tenerife, España';
+
 }
